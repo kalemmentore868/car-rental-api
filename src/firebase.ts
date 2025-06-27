183517fecd3cfb933fcd314811751953fe2f388f
Kalem Mentore <kalem@Kalems-MacBook-Air.local>
2025-06-27 15:10:20 -0400

    fixed env

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,19 @@
 import * as firebase from "firebase-admin";
-import * as serviceAccount from "./car-rental.json";
 import { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const params = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientC509CertUrl: serviceAccount.client_x509_cert_url,
+  type: process.env.SERVICE_ACCOUNT_TYPE,
+  projectId: process.env.PROJECT_ID,
+  privateKeyId: process.env.PRIVATE_KEY_ID,
+  privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"), // fix escaped newlines
+  clientEmail: process.env.CLIENT_EMAIL,
+  clientId: process.env.CLIENT_ID,
+  authUri: process.env.AUTH_URI,
+  tokenUri: process.env.TOKEN_URI,
+  authProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  clientC509CertUrl: process.env.CLIENT_X509_CERT_URL,
 };
 
 firebase.initializeApp({
